1b8c1f12400b20dbc82f2a7f154850f8e9c1b71e
armytag <armitageth@gmail.com>
2016-02-18 08:09:07 +0800

    Version 0.8 commit.  Structure and functionality complete.

---
+++ script.js
@@ -0,0 +1,17 @@
+var min_height = "120px";
+
+document.querySelector('#rules').style.maxHeight = min_height;
+document.querySelector('#signup').style.maxHeight = min_height;
+document.querySelector('#stories').style.maxHeight = "720px";
+
+function expand (obj) {
+	console.log(obj.style.maxHeight +" "+ obj.className);
+	if (obj.style.maxHeight == min_height) {
+		obj.style.maxHeight = "720px";
+		obj.className = "panel open";
+	}else {
+		obj.style.maxHeight = min_height;
+		obj.className = "panel";
+	};
+	console.log(obj.style.maxHeight +" "+ obj.className);
+}
